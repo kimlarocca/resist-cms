41efb1c8db260328975502924ca5c196b206e289
Kim LaRocca <kim@kimlarocca.com>
2025-10-20 21:32:05 -0400

    request ui update

--- pages/request-a-sign.vue
+++ pages/request-a-sign.vue
@@ -17,18 +17,29 @@ onMounted(() => {
 </script>
 <template>
   <div class="container px-4 xl:px-0 pt-4 lg:pt-8">
-    <h1 class="mb-4">Request A Yard Sign</h1>
-    <h2 class="mb-4">Spread the Word - Jack Ciattarelli Can't Fix Jack!</h2>
-    <img
-      class="mb-6"
-      src="/images/yard-sign.jpg"
-      alt="Can't Fix Jack Yard Sign"
-    />
-    <iframe
-      aria-label="request a sign form"
-      style="border: none; width: 100%; height: 700px"
-      id="request-a-sign-pcxro7"
-      src="https://noteforms.com/forms/request-a-sign-pcxro7"
-    ></iframe>
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-16">
+      <div>
+        <h1>Request A Yard Sign</h1>
+        <h2 class="mb-6">Spread the Word - Jack Ciattarelli Can't Fix Jack!</h2>
+        <img
+          class="mb-6"
+          src="/images/yard-sign.jpg"
+          alt="Can't Fix Jack Yard Sign"
+        />
+        <iframe
+          aria-label="request a sign form"
+          style="border: none; width: 100%; height: 700px"
+          id="request-a-sign-pcxro7"
+          src="https://noteforms.com/forms/request-a-sign-pcxro7"
+        ></iframe>
+      </div>
+      <div class="hidden lg:block text-right">
+        <img
+          src="/images/PuppetJack.jpg"
+          alt="Donald Trump's Puppet - Jack the Flipper"
+          class="w-full lg:w-fit"
+        />
+      </div>
+    </div>
   </div>
 </template>
