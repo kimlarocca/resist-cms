8ad1e852ee55f70192da56849a6c61c5cefc94d6
Kim LaRocca <kim@kimlarocca.com>
2025-11-22 11:58:45 -0500

    footer tweaks

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,44 +1,10 @@
 <script setup>
 const currentYear = new Date().getFullYear()
-const socialLinks = [
-  { icon: "pi pi-link", url: "https://linktr.ee/papnj", label: "Linktree" },
-  {
-    icon: "pi pi-tiktok",
-    url: "https://tiktok.com/@newjerseypeople",
-    label: "TikTok",
-  },
-  {
-    icon: "pi pi-facebook",
-    url: "https://www.facebook.com/NewJerseyPeople",
-    label: "Facebook",
-  },
-  {
-    icon: "pi pi-twitter",
-    url: "https://x.com/NewJerseyPeople",
-    label: "X (Twitter)",
-  },
-  {
-    icon: "pi pi-instagram",
-    url: "https://instagram.com/NJPeopleArePeople",
-    label: "Instagram",
-  },
-  {
-    icon: "custom",
-    iconSrc: "/bluesky.svg",
-    url: "https://bsky.app/profile/newjerseypeople.bsky.social",
-    label: "Bluesky",
-  },
-  {
-    icon: "pi pi-youtube",
-    url: "https://www.youtube.com/@PeopleArePeopleNJ",
-    label: "YouTube",
-  },
-]
 </script>
 
 <template>
-  <footer class="bg-white-opacity-70 rounded-tl-xl rounded-tr-xl">
-    <div class="container p-12 text-center">
+  <footer class="bg-white-opacity-70 container rounded-xl">
+    <div class="py-8 px-4 lg:px-24 text-center">
       <p class="small mb-3">
         This website was not requested or authorized by any candidate or candidate's
         committee. Paid for by
