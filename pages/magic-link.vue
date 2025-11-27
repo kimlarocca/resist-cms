706164ee85be09c8fb54a9ff06c7f4043b4c41e7
Kim LaRocca <kim@kimlarocca.com>
2025-11-27 11:29:47 -0500

    magic link tweak

--- pages/magic-link.vue
+++ pages/magic-link.vue
@@ -8,13 +8,8 @@
     <Logo class="mb-3 ml-1" />
     <hr class="w-44 ml-0 mb-8" />
     <h1 class="mb-6">Magic Link</h1>
-    <template v-if="errorMessage">
-      <Message class="mb-4" severity="error">
-        Sorry, there was a problem logging in to your account.
-      </Message>
-    </template>
     <p>
-      <a href="/">
+      <a href="/dashboard">
         Please wait a few moments. If you are not automatically logged in, click here!
         Your magic link may have expired.
       </a>
@@ -22,32 +17,15 @@
   </div>
 </template>
 
-<script setup>
-// import { useCurrentUser } from "~/composables/states"
-// const currentUser = useCurrentUser()
-
-definePageMeta({
-  layout: "blank",
-})
-
-// // check the dev environment for the auth token
-// const supabaseAuthTokenDev = JSON.parse(
-//   window.localStorage.getItem('sb-dltcuuoimfzoticynpzy-auth-token')
-// )
-// if (supabaseAuthTokenDev) {
-//   currentUser.value = supabaseAuthTokenDev.user
-// }
-
-// // check the prod environment for the auth token
-// const supabaseAuthTokenProd = JSON.parse(
-//   window.localStorage.getItem('sb-brwzzilslduwostxbufg-auth-token')
-// )
-// if (supabaseAuthTokenProd) {
-//   currentUser.value = supabaseAuthTokenProd.user
-// }
-
-// for some reason, navigateTo doesn't work here?!
-onMounted(() => {
-  window.location.href = "/dashboard"
-})
+<script setup lang="ts">
+const user = useSupabaseUser()
+watch(
+  user,
+  () => {
+    if (user.value) {
+      return navigateTo("/dashboard")
+    }
+  },
+  { immediate: true }
+)
 </script>
