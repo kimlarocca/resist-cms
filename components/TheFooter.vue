209b4460baca171566995c64f8ea4b01ed4f778a
Kim LaRocca <kim@kimlarocca.com>
2025-11-27 12:12:55 -0500

    more footer tweaks

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,8 +3,8 @@ const currentYear = new Date().getFullYear()
 </script>
 
 <template>
-  <footer class="container p-4">
-    <div class="bg-gray rounded-xl p-4 text-center">
+  <footer class="bg-gray rounded-t-xl">
+    <div class="container p-4 text-center">
       <p class="text-sm">
         Copyright © {{ currentYear }} ResistCMS.com. All Rights Reserved.
       </p>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>
+  <div class="default-container flex flex-col h-full">
     <TheHeader />
     <main class="mb-8">
       <NuxtPage />
     </main>
-    <TheFooter />
+    <TheFooter class="mt-auto" />
   </div>
 </template>
+
+<style scoped>
+.default-container {
+  min-height: 100vh;
+}
+</style>
